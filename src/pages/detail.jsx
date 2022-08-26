18d8256d5ba8017276597689a328833a0a6bb891
m1nsuppp <m1nsuppp@gmail.com>
2022-08-26 23:14:11 +0900

    design: 뒤로가기 화살표 스타일링

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,13 +1,25 @@
 // 상세
+import styled from "@emotion/styled";
+
+import { colors } from "../lib/constants/colors";
 import DetailContainer from "../components/detail/DetailContainer";
+import Header from "../components/common/header/Header";
+
+const ArrowBackStyle = styled.div`
+  padding: 8px 12px;
+  color: red;
+  font-size: 24px;
+`;
 
 const Detail = () => {
   return (
     <div>
-      <header>
-        <div>this is header</div>
-      </header>
-      <div>뒤로 가기</div>
+      <Header />
+      <ArrowBackStyle>
+        <span className="material-symbols-outlined" style={ { color: colors.red, fontSize: "36px", } }>
+          arrow_back
+        </span>
+      </ArrowBackStyle>
       <DetailContainer />
     </div>
   );
