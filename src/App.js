28a218316c98777d96004bb16a00368e2863ed99
m1nsuppp <m1nsuppp@gmail.com>
2022-08-26 13:50:53 +0900

    feat: routing path setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-function App() {
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+
+import Home from "../src/pages/index";
+import Detail from "../src/pages/detail";
+import Write from "../src/pages/write";
+
+const App = () => {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/:id" element={ <Detail /> } />
+        <Route path="/write" element={ <Write /> } />
+      </Routes>
     </div>
   );
 }
