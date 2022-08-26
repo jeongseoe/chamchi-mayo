c836d4be64b9b41c3dfea13e655942815d49c94c
jeongseoe <gldi1633@naver.com>
2022-08-26 22:06:14 +0900

    feat : pages Router 추가

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from "../src/pages/index";
 import Detail from "../src/pages/detail";
 import Write from "../src/pages/write";
+import Posts from "./pages/posts";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
         <Route path="/posts:id" element={<Detail />} />
         <Route path="/write" element={<Write />} />
       </Routes>
