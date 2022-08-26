9316ab50b4c9803a4b1aca55c703808af0c42b86
m1nsuppp <m1nsuppp@gmail.com>
2022-08-26 14:05:09 +0900

    style: routing path 확인을 위한 변경

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,7 +1,12 @@
 // 상세
+import CommonButton from "../components/common/button/CommonButton";
 
 const Detail = () => {
-
+  return (
+    <CommonButton>
+      <span>여기는 상세페이지입니다!</span>
+    </CommonButton>
+  );
 };
 
 export default Detail;
--- src/pages/write.jsx
+++ src/pages/write.jsx
@@ -1,7 +1,12 @@
 // 글쓰기
+import CommonButton from "../components/common/button/CommonButton";
 
 const Write = () => {
-
+  return (
+    <CommonButton>
+      <span>여기는 업로드 페이지입니다!</span>
+    </CommonButton>
+  );  
 };
 
 export default Write;
