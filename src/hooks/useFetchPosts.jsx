8999eb10f14761459e3fdf558c22e803dbb19779
m1nsuppp <m1nsuppp@gmail.com>
2022-08-27 18:02:04 +0900

    refactor: parameter `boolean`의 필요성 부재로 삭제

--- src/hooks/useFetchPosts.jsx
+++ src/hooks/useFetchPosts.jsx
@@ -2,18 +2,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 /**
- * 
- * @param {*} postId: `/posts`에 대해선 null값 지정(정서님)
- * @param {*} boolean: `/posts`에 대해선 true입니다(정서님)
- * posts가 맞냐 아니냐로 구분하겠습니다.
- * 예를 들어, `/posts/1`과 같은 경우, 하나(단수형)의 post만 요청하기 때문에 false입니다.
- * `/posts`의 경우는 여러개(복수형)의 post를 요청하기 때문에 `posts`가 맞습니다. 따라서 true입니다.
+ * `/posts/1`과 같이 postId가 있다면 값을 주고, 
+ * 정서님이 개발하시는 컴포넌트와 같이 postId가 없는 경우, null을 지정해주면 됩니다.
+ * @param {*} postId
+ *                  
+ * @returns 
  */
-const useFetchPosts = (postId, boolean) => {
+const useFetchPosts = (postId) => {
   const [posts, setPosts] = useState(null);
 
   const fetchPosts = async () => {
-    const URL = boolean ? `http://localhost:5001/posts` : `http://localhost:5001/posts/${postId}`;
+    const URL = postId ? `http://localhost:5001/posts/${postId}` : `http://localhost:5001/posts`;
     const response = await axios.get(URL).catch(error => console.log(error));
     
     setPosts(response.data);
