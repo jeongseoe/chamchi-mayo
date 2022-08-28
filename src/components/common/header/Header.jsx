e2e958afd5189c7f62754d3ddecf106c82e82953
m1nsuppp <m1nsuppp@gmail.com>
2022-08-28 16:36:02 +0900

    refactor: 가독성을 위해 `inline style`제거

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -3,6 +3,19 @@ import { Link } from "react-router-dom";
 
 import { colors } from "../../../lib/constants/colors";
 
+const Header = () => {
+  return (
+    <HeaderStyle color={ colors.blue }>
+      <Link to="/">
+        <span className="material-symbols-outlined">
+          home
+        </span>
+      </Link>
+      <h1>화난 그대, 참치마요.</h1>
+    </HeaderStyle>
+  );
+};
+
 const HeaderStyle = styled.header`
   display: flex;
   justify-content: space-between;
@@ -11,20 +24,10 @@ const HeaderStyle = styled.header`
   height: 10vh;
   padding: 8px 12px;
   pointer: cursor;
+  span {
+    font-size: 36px;
+    color: ${colors.red};
+  }
 `;
-
-const Header = () => {
-  return (
-    <HeaderStyle color={ colors.blue }>
-      <Link to="/">
-        <span className="material-symbols-outlined" 
-              style={ { color: colors.red, fontSize: "36px", } }
-              >home
-        </span>
-      </Link>
-      <h1>화난 그대, 참치마요.</h1>
-    </HeaderStyle>
-  );
-};
 
 export default Header;
