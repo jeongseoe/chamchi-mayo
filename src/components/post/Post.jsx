cc14e7d275803968df2fff30360c9c53f10c719c
m1nsuppp <m1nsuppp@gmail.com>
2022-08-31 16:09:31 +0900

    feat: `Infinit Scroll` 구현
    하지만, 이 방법이 그닥 옳지않은 것 같음
    `isIntersection`일 때, 백엔드로부터 데이터를 추가요청해야할 것 같음

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,31 +1,63 @@
 import styled from "@emotion/styled";
+import React, { useEffect, useRef, useState } from "react";
+
 import useFetchPost from "../../hooks/useFetchPosts";
 import List from "../post/List";
 
 const PostTitle = styled.div`
-font-size: 40px;
-color:#FF0000;
-text-align: center;
-margin-top: 20px;
-margin-bottom: 20px;
+  font-size: 40px;
+  color:#FF0000;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
 `;
 
-
 const Post = () => {
-    const posts = useFetchPost(null);
+  const boxRef = useRef();
+  const [page, setPage] = useState(1);
+  const posts = useFetchPost(null, page);
 
-    return (
-        <>
-            <div>
-                <PostTitle>분노목록</PostTitle>
-                <div>
-                    {posts?.map((post) => {
-                        return <List key={post.id} post={post} />
-                    })}
-                </div>
-            </div>
-        </>
-    )
+  const onIntersect = (entries, observer) => {
+    const [ entry ] = entries;
+    // setIsVisible(entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setPage(prev => prev + 1);
+      console.log('지금 데이터 더 불러오삼 ㅋㅋ');
+    }
+  };
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, options);
+    if (boxRef.current) {
+      observer.observe(boxRef.current);
+    }
+
+    return () => {
+      if (boxRef.current) {
+        observer.unobserve(boxRef.current);
+      }
+    }
+  }, [boxRef, options]);
+
+  console.log(page);
+
+  return (
+    <>
+      <div>
+        <PostTitle>분노목록</PostTitle>
+        <div>
+          {posts && posts.slice(0, page * (10)).map((post) => {
+            return <List ref={boxRef} key={post.id} post={post} />
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Post;
