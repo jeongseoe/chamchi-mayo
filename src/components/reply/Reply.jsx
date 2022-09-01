842f4ed30c5dab9017519c3e0557bd840d811261
m1nsuppp <m1nsuppp@gmail.com>
2022-09-01 11:35:54 +0900

    feat: merge branch `dahyun` into `master`

--- src/components/reply/Reply.jsx
+++ src/components/reply/Reply.jsx
@@ -1,104 +1,125 @@
 import React, { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Reply = () => {
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [body, setBody] = useState("");
-    const [replies, setReplies] = useState(null); //서버에서 받아온 데이터
-    const [editBody, setEditBody] = useState({
-            body: "",
-        });
-    const handlerChangeInput = (e, setState) => setState(e.target.value);
-
-    const handlerSubmit = async(e) => {
-        e.preventDefault();
-        const URL = 'http://localhost:5001/reply';
-        const data = {
-            "name":name,
-            "body":body,
-            "password":password
-        };
-        const res = await axios.post(URL, data);
-        setPassword('');
-        setName('');
-        setBody('');
+  const params = useParams();
+  const id = params.id;
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [body, setBody] = useState("");
+  const [replies, setReplies] = useState([]); //서버에서 받아온 데이터
+  const [editBody, setEditBody] = useState({ body: "", });
+  const handlerChangeInput = (e, setState) => setState(e.target.value);
+  
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    const URL = `http://localhost:5001/reply`;
+    const data = {
+      "id": replies ? replies.length : 0,
+      "name": name,
+      "body": body,
+      "password": password
     };
 
-    console.log(replies);
+    const afterInsertReplies = replies.concat(data);
+    setReplies(afterInsertReplies);
+
+    await axios.patch(URL, { "replies": afterInsertReplies }).catch(error => error);
+
+    setPassword('');
+    setName('');
+    setBody('');
+  };
+
+  // only mounted
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const URL = `http://localhost:5001/reply`;
 
     const fetchReply = async () => {
-        const URL = 'http://localhost:5001/reply';
-        const response = await axios.get(URL);
-        setReplies(response.data)
-    }
+      try {
+        const res = await axios.get(URL, {
+          CancelToken: source.token,
+        });
+        console.log(res.data[id]);
+        setReplies(res.data[id]);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+        } else {
+          throw error;
+        }        
+      }
+    };
 
-    useEffect(()=>{
-        fetchReply();
-    },[name, body, password])
+    fetchReply();
 
-    const handlerEdit = async (id, edit) => {
-        const {data} = await axios.patch(`http://localhost:3001/reply/${id}`, edit);
-        setReplies(data)
-    }
+    return () => source.cancel();
+  }, []);
 
-    const handlerDelete = (replyId) => {
-        axios.delete(`http://localhost:3001/reply/${replyId}`);
-    }    
-    console.log(name, body, password)
+  const handlerEdit = async (replyId) => {
+    const afterUpdateReplies = replies.map(reply => reply.id === replyId ? { ...reply, body: editBody } : reply);
 
-    return (
-        <>
-            <form onSubmit={handlerSubmit}>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="이름"
-                    value = {name}
-                    onChange={(e) => handlerChangeInput(e, setName)}
-                />
-                <input
-                    type="text"
-                    name="body"
-                    placeholder="댓글"
-                    value = {body}
-                    onChange={(e) => handlerChangeInput(e, setBody)}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="비밀번호"
-                    value = {password}
-                    onChange={(e) => handlerChangeInput(e, setPassword)}
-                />
-                <button type="submit"
-                >등록하기</button>
-            </form>
+    setReplies(afterUpdateReplies);
+  };
 
-            <div>
-                {replies && replies.map(reply => {
-                    console.log(replies)
-                    return <div key={reply.id}>
-                        <div>이름 :{reply.name} 댓글 :{reply.body}</div>
-                        <input 
-                        type="text"
-                        placeholder="댓글수정"
-                        onChange={(e)=> setEditBody({...editBody, body:e.target.value})}
-                        />
-                        <button
-                         type="button"
-                         onClick={() => handlerEdit(reply.id, editBody)}   
-                        >수정</button>
-                        <button
-                        type="button"
-                        onClick={() => handlerDelete(reply.id)}
-                        >삭제</button>
-                    </div>
-                })}
-            </div>
-        </>
-    );
+  const handlerDelete = async (replyId) => {
+    const afterDeleteReplies = replies.filter(reply => reply.id !== replyId);
+
+    setReplies(afterDeleteReplies);
+  };
+
+  return (
+    <>
+      <form onSubmit={handlerSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={name}
+          onChange={(e) => handlerChangeInput(e, setName)}
+        />
+        <input
+          type="text"
+          name="body"
+          placeholder="댓글"
+          value={body}
+          onChange={(e) => handlerChangeInput(e, setBody)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => handlerChangeInput(e, setPassword)}
+        />
+        <button type="submit"
+        >등록하기</button>
+      </form>
+
+      <div>
+        {replies && replies.map(reply => {
+          return <div key={reply.id}>
+            <div>{reply.id}이름 :{reply.name} 댓글 :{reply.body}</div>
+            <input
+              type="text"
+              placeholder="댓글수정"
+              onChange={(e) => setEditBody(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={() => handlerEdit(reply.id, editBody)}
+            >수정</button>
+            <button
+              type="button"
+              onClick={() => handlerDelete(reply.id)}
+            >삭제</button>
+          </div>
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Reply;
