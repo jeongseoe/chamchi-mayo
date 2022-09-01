9bfde446a5823697fb402317fa4cd48e1628b681
jeongseoe <109732299+jeongseoe@users.noreply.github.com>
2022-09-01 12:18:56 +0900

    Update List.jsx

--- src/components/post/List.jsx
+++ src/components/post/List.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 
-const List = (props) => {
-    const navigate = useNavigate();
-    return (
-        <div>
-            <StContainer>
-                <StBox onClick={() => {
-                    navigate(`/posts/${props.post.id}`);
-                }}>
-                    <p style={{ fontSize: "25px", color: "#FD7F20", marginTop: "5px" }}>
-                        {props.post.title}
-                    </p>
-                    <p style={{ fontSize: "25px", color: "#FF0000", marginTop: "5px" }}>
-                        {props.post.writer}
-                    </p>
-                </StBox>
-            </StContainer>
-        </div >
-    );
-}
+import { colors } from "../../lib/constants/colors";
+
+const List = forwardRef((props, ref) => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <StContainer>
+        <StBox ref={ref} onClick={() => {
+          navigate(`/posts/${props.post.id}`);
+        }}>
+          <p style={{ color: colors.orange }}>
+            {props.post.title}
+          </p>
+          <p style={{ marginLeft: "50px", color: colors.red, marginTop: "5px" }}>
+            {props.post.writer}
+          </p>
+        </StBox>
+      </StContainer>
+    </div >
+  );
+});
 
 export default List;
 
